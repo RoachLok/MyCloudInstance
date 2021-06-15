3c63f2d54ed14272061f1c6aea2b7b12e65fae1c
RoachLok <jtabordajunior@hotmail.com>
2021-06-15 02:28:06 +0200

    Added mapview endpoints to api.php with required token verification.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'is_admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
-        'token_auth' => \Illuminate\Auth\Middleware\EnsureTokenIsValid::class,
+        'token_auth' => \App\Http\Middleware\EnsureTokenIsValid::class,
     ];
 }
